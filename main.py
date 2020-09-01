6f9cee2775b06226583b826e681f7f476bb0b8db
Aaron C Enten <48533611+aenten1@users.noreply.github.com>
2020-09-01 11:03:15 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+import sqlalchemy
+from sqlalchemy.orm import Session
+#from pydantic import BaseModel
+#from database import SessionLocal, engine
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def dashboard(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request
+        })
+
+
